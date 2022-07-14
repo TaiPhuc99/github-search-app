bb797de10ad98b11f1fd9a91fe29b5e05ec5780c
TaiPhuc99 <taitruong8999.it@gmail.com>
2022-07-14 17:40:09 +0700

    fixed service

--- src/services/GithubService.js
+++ src/services/GithubService.js
@@ -1,28 +1,31 @@
 import axios from "axios";
-import { BASE_URL, GITHUB_TOKEN } from "./ConfigURL";
+import { BASE_URL } from "./ConfigURL";
 
 export const GithubService = {
   searchDataUsers: (param) => {
-    return axios.get(`${BASE_URL}/search/users?${param}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
+    return axios.get(`${BASE_URL}/search/users?${param}`);
+    //  {
+    //   headers: {
+    //     Authorization: `token ${GITHUB_TOKEN}`,
+    //   },
+    // });
   },
 
   getSingleUser: (username) => {
-    return axios.get(`${BASE_URL}/users/${username}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
+    return axios.get(`${BASE_URL}/users/${username}`);
+    // {
+    //   headers: {
+    //     Authorization: `token ${GITHUB_TOKEN}`,
+    //   },
+    // });
   },
 
   getUserRepo: (username) => {
-    return axios.get(`${BASE_URL}/users/${username}/repos`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
+    return axios.get(`${BASE_URL}/users/${username}/repos`);
+    //  {
+    //   headers: {
+    //     Authorization: `token ${GITHUB_TOKEN}`,
+    //   },
+    // });
   },
 };
